b8f42b92dfc2902329677ca82e4fa0e8847e1ab3
Fuad <s233468@dtu.dk>
2025-11-17 12:48:54 +0100

    removed main.rs

---
+++ target/debug/deps/vikarin_workers-164fd85d7d0527b2.d
@@ -0,0 +1,5 @@
+C:\Users\fuad_\Documents\vikarin-backend\target\debug\deps\vikarin_workers-164fd85d7d0527b2.d: src\main.rs
+
+C:\Users\fuad_\Documents\vikarin-backend\target\debug\deps\libvikarin_workers-164fd85d7d0527b2.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/vikarin_workers-2702ce13b11d573c.d
@@ -0,0 +1,5 @@
+C:\Users\fuad_\Documents\vikarin-backend\target\debug\deps\vikarin_workers-2702ce13b11d573c.d: src\main.rs
+
+C:\Users\fuad_\Documents\vikarin-backend\target\debug\deps\libvikarin_workers-2702ce13b11d573c.rmeta: src\main.rs
+
+src\main.rs:
